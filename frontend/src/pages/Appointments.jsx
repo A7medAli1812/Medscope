19eb5862456af42695f26d4c4e3b0e712baf2e3d
ZiadUchiha <ziad.mahmoud.medscope@gamil.com>
2025-12-20 16:57:41 +0200

    test

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import PlaceholderPage from '../components/PlaceholderPage';
 import "./Appointments.css"
 const Appointments = () => {
-    return <PlaceholderPage title="Appointments" />;
+    return <div>
+        <h1>Appointments Page</h1>
+        <PlaceholderPage title="Appointments" />
+    </div>;
 };
 
 export default Appointments;
