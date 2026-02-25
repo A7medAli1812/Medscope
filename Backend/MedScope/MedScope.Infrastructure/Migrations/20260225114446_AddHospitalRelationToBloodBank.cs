b11a3de2ec4e5201947e2c6ef293f94d258b9453
Tasneem Khaled <tasneem.khaled.medscope@gmail.com>
2026-02-25 15:08:04 +0200

    Admin Blood Bank

---
+++ Backend/MedScope/MedScope.Infrastructure/Migrations/20260225114446_AddHospitalRelationToBloodBank.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedScope.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHospitalRelationToBloodBank : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HospitalId",
+                table: "BloodBanks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BloodBanks_HospitalId",
+                table: "BloodBanks",
+                column: "HospitalId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BloodBanks_Hospitals_HospitalId",
+                table: "BloodBanks",
+                column: "HospitalId",
+                principalTable: "Hospitals",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BloodBanks_Hospitals_HospitalId",
+                table: "BloodBanks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BloodBanks_HospitalId",
+                table: "BloodBanks");
+
+            migrationBuilder.DropColumn(
+                name: "HospitalId",
+                table: "BloodBanks");
+        }
+    }
+}
---
+++ Backend/MedScope/MedScope.Infrastructure/Migrations/20260225122751_AddUniqueConstraintToBloodBank.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedScope.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueConstraintToBloodBank : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BloodType",
+                table: "BloodBanks",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BloodBanks_BloodType_HospitalId",
+                table: "BloodBanks",
+                columns: new[] { "BloodType", "HospitalId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BloodBanks_BloodType_HospitalId",
+                table: "BloodBanks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BloodType",
+                table: "BloodBanks",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
