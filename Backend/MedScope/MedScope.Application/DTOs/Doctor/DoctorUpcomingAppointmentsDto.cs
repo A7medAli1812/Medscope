360739a48b0de0581c5a469b4c7588e604ced723
Malakmostafa220 <malak.mostafa.medscope@gmail.com>
2026-03-02 01:43:49 +0200

    Doctor Dashbored

---
+++ Backend/MedScope/MedScope.Application/DTOs/Doctor/DoctorUpcomingAppointmentsDto.cs
@@ -0,0 +1,14 @@
+﻿public class DoctorUpcomingAppointmentsDto
+{
+    public int AppointmentId { get; set; }
+
+    public string Time { get; set; }
+    public string Date { get; set; }
+
+    public string PatientName { get; set; }
+    public int PatientAge { get; set; }
+
+    public string VisitType { get; set; }
+
+    public string HospitalName { get; set; } // 🔥 مهم
+}
