bc0906dea12fa5748f06d15a2f4fbeab3c63e3b8
Tasneem Khaled <tasneem.khaled.medscope@gmail.com>
2026-04-14 01:56:34 +0200

    Obj and bin and debug files

--- Backend/MedScope/MedScope.Domain/obj/Debug/net9.0/MedScope.Domain.AssemblyInfo.cs
+++ Backend/MedScope/MedScope.Domain/obj/Debug/net9.0/MedScope.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedScope.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b183c5a1ae8a60fe0e13b74c41ba174a134910e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bb14bf58a62b089d39c1a3eef41ff10553c99a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedScope.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedScope.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
