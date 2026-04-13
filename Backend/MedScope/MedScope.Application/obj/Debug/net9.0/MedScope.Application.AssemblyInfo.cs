cdb93da507ab393b32390fde665ca87e5b5a0dbc
Tasneem Khaled <tasneem.khaled.medscope@gmail.com>
2026-04-13 22:52:34 +0200

    remove obj files

--- Backend/MedScope/MedScope.Application/obj/Debug/net9.0/MedScope.Application.AssemblyInfo.cs
+++ Backend/MedScope/MedScope.Application/obj/Debug/net9.0/MedScope.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedScope.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e67e4a5991e071a09b33b0d139f7a505f0183a33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dc52e869e026ecbfe21f7eac7b4d3f6b2afcae6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedScope.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedScope.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
