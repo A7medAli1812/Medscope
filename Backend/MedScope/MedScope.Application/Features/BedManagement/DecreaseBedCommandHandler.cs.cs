aa714f2d0c84eadb80ca955057ee6e76743f7e74
Tasneem Khaled <tasneem.khaled.medscope@gmail.com>
2026-06-17 13:50:45 +0300

    last not least

--- Backend/MedScope/MedScope.Application/Features/BedManagement/DecreaseBedCommandHandler.cs.cs
+++ Backend/MedScope/MedScope.Application/Features/BedManagement/DecreaseBedCommandHandler.cs.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +9,7 @@ using MedScope.Application.Abstractions.Persistence;
 
 namespace MedScope.Application.Features.BedManagement
 {
-    public class DecreaseBedCommandHandler : IRequestHandler<DecreaseBedCommand, Unit>
+    public class DecreaseBedCommandHandler : IRequestHandler<DecreaseBedCommand, int>
     {
         private readonly IApplicationDbContext _context;
 
@@ -18,7 +18,7 @@ namespace MedScope.Application.Features.BedManagement
             _context = context;
         }
 
-        public async Task<Unit> Handle(DecreaseBedCommand request, CancellationToken cancellationToken)
+        public async Task<int> Handle(DecreaseBedCommand request, CancellationToken cancellationToken)
         {
             var bed = await _context.Beds.FindAsync(request.Id);
 
@@ -28,9 +28,9 @@ namespace MedScope.Application.Features.BedManagement
             if (bed.AvailableBeds > 0)
                 bed.AvailableBeds--;
 
-            await _context.SaveChangesAsync(cancellationToken);
+            var rows = await _context.SaveChangesAsync(cancellationToken);
 
-            return Unit.Value;
+            return rows;
         }
     }
 }
