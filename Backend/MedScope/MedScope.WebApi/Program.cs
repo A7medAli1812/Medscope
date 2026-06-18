63fb38613c093a1a97ea0c087d70833b93e1d873
Tasneem Khaled <tasneem.khaled.medscope@gmail.com>
2026-06-19 02:04:42 +0300

    last of last

--- Backend/MedScope/MedScope.WebApi/Program.cs
+++ Backend/MedScope/MedScope.WebApi/Program.cs
@@ -26,6 +26,12 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// =======================
+// 🔥 لمنع تأثير ملف appsettings.Production.json الذي يتولد على السيرفر
+// نقوم بإعادة تحميل appsettings.json ليكون له الأولوية القصوى
+// =======================
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
 builder.Services.Configure<EmailSettings>(
     builder.Configuration.GetSection("EmailSettings"));
 
